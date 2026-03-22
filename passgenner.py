e99fd6822c7cd7bf23cc0a7448e1c860610784ca
Manmade <267263224+Manmade9416@users.noreply.github.com>
2026-03-22 11:07:58 +0200

    Made it possible to generate passwords containing no nums or special characters or both. Also better commenting

--- passgenner.py
+++ passgenner.py
@@ -4,11 +4,15 @@ import secrets
 import string
 
 def password_gen(length, minnum, special):
+    """ Generate Strong Passwords."""
     # Prevent minnum + special >= length
+    if length <= 3:
+        raise ValueError("Password length too short.")
     if minnum + special >= length:
         raise ValueError("Total minimum requirements exceed password length.")
     
     # Character pools
+    letters = string.ascii_letters
     amanumber = string.digits
     amaspecial = string.punctuation
     alphabet = string.digits + string.ascii_letters + string.punctuation
@@ -16,28 +20,58 @@ def password_gen(length, minnum, special):
     # Password character storage
     password_chars = []
     
-    # Numbers to satisfy minnum 
-    if minnum > 0: 
+    # Checks to satisfy special characters and numbers requirements
+    ## Some nums and some special chars
+    if minnum > 0 and special > 0:  
         for i in range(minnum):
             password_chars.append(secrets.choice(amanumber))
-    
-    # Special chars to satisfy special
-    if special > 0:
         for i in range(special):
             password_chars.append(secrets.choice(amaspecial))
-    
-    # Fill remaining length of the password
-    remaining = length - minnum - special
-    for i in range(remaining):
-        password_chars.append(secrets.choice(alphabet))
+
+        remaining = length - minnum - special
+        for i in range(remaining):
+            password_chars.append(secrets.choice(alphabet))
+
+        # Shuffle the Password chars and return password
+        random.SystemRandom().shuffle(password_chars)
+        return ''.join(password_chars)
+
+    ## Some nums but no special chars
+    elif minnum > 0 and special < 1:
+        for i in range(minnum):
+            password_chars.append(secrets.choice(amanumber))
+
+        remaining = length - minnum
+        for i in range(remaining):
+            password_chars.append(secrets.choice(amanumber + letters))
+
+        # Shuffle the Password chars and retun password
+        random.SystemRandom().shuffle(password_chars)
+        return ''.join(password_chars)
+
+    ## Some special chars but no nums
+    elif minnum < 1 and special > 0: 
+        for i in range(special):
+            password_chars.append(secrets.choice(amaspecial))
         
-    # Shuffle the Password Characters
-    random.SystemRandom().shuffle(password_chars)
-    
-    return ''.join(password_chars)
+        remaining = length - special
+        for i in range(remaining):
+            password_chars.append(secrets.choice(amaspecial + letters))
+
+        # Shuffle the Password chars and retun password
+        random.SystemRandom().shuffle(password_chars)
+        return ''.join(password_chars)
+
+    ## Just letters, no nums and special chars
+    elif minnum < 1 and special < 1:
+        return ''.join(secrets.choice(letters) for i in range(length))
     
 
 def main():
+    """
+    Passes the arguments into the password_gen()
+    and prints out the password.
+    """
     parser = argparse.ArgumentParser(
         description="Generate strong Passwords.")
     parser.add_argument("--Length", "-L", type=int, help="Length of Password. Default(64)", default=64)
